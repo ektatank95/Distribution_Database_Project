d3bbdf91b0be5ccef546fc0075f0ac238947fe65
ektatank <ektatank95@gmail.com>
2020-03-20 21:56:40 +0530

    Second commit

--- src/GreedyAllocationAlgorithm.java
+++ src/GreedyAllocationAlgorithm.java
@@ -1,7 +1,9 @@
+
 public class GreedyAllocationAlgorithm {
 
     public static void main(String args[]){
         System.out.println("HI Tesing program");
+        System.out.println("I am in");
     }
 
 }
