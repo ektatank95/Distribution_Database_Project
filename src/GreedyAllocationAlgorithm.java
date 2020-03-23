188aa925839c15a2985df57ab5f14691fc74a85e
ektatank <ektatank95@gmail.com>
2020-03-23 13:50:14 +0530

    connection created.. and create table query by java code

--- src/GreedyAllocationAlgorithm.java
+++
@@ -1,9 +0,0 @@
-
-public class GreedyAllocationAlgorithm {
-
-    public static void main(String args[]){
-        System.out.println("HI Tesing program");
-        System.out.println("I am in");
-    }
-
-}
