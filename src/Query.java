28af1fb66904ea1e46e04800b1564d6c69951700
ektatank <ektatank95@gmail.com>
2020-03-24 12:58:39 +0530

    Query weight calculated, frequency parameter added

--- src/Query.java
+++ src/Query.java
@@ -5,12 +5,53 @@ public class Query {
     private String query;
     private Double queryCost;
     private List<String> tableUsed;
+    private Integer frequency;
+    private List<Query> updates;
+    private Double weight;
+    private boolean transcationalQuery;
 
-    public Query(String queryId, String query, Double queryCost, List<String> tableUsed) {
+
+    public Query(String queryId, String query, Double queryCost, List<String> tableUsed, Integer frequency, List<Query> updates, Double weight, boolean transcationalQuery) {
         this.queryId = queryId;
         this.query = query;
         this.queryCost = queryCost;
         this.tableUsed = tableUsed;
+        this.frequency = frequency;
+        this.updates = updates;
+        this.weight = weight;
+        this.transcationalQuery = transcationalQuery;
+    }
+
+    public Integer getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(Integer frequency) {
+        this.frequency = frequency;
+    }
+
+    public List<Query> getUpdates() {
+        return updates;
+    }
+
+    public void setUpdates(List<Query> updates) {
+        this.updates = updates;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isTranscationalQuery() {
+        return transcationalQuery;
+    }
+
+    public void setTranscationalQuery(boolean transcationalQuery) {
+        this.transcationalQuery = transcationalQuery;
     }
 
     public String getQueryId() {
@@ -52,6 +93,10 @@ public class Query {
                 ", query='" + query + '\'' +
                 ", queryCost=" + queryCost +
                 ", tableUsed=" + tableUsed +
+                ", frequency=" + frequency +
+                ", updates=" + updates +
+                ", weight=" + weight +
+                ", transcationalQuery=" + transcationalQuery +
                 '}';
     }
 }
--- src/SelectQueryAnalysis.java
+++ src/SelectQueryAnalysis.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class SelectQueryAnalysis {
 
-    public static List<String> findTableRequired(String query) {
+    public static List<String> findAllTableOfDatabase() {
         List<String> tableList = new ArrayList<>();
         ResultSet rs;
         try {
             rs = UtlityClass.getConnection().executeQuery(QueryConstant.TABLE_REQUIRED);
             while (rs.next()) {
-                tableList.add(rs.getString("table_name"));
+                tableList.add(rs.getString("table_name").toLowerCase());
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -24,10 +24,10 @@ public class SelectQueryAnalysis {
         ResultSet rs;
         double queryCost = 0;
         try {
-            rs = UtlityClass.getConnection().executeQuery(QueryConstant.EXPLAIN+" " + query);
+            rs = UtlityClass.getConnection().executeQuery(QueryConstant.EXPLAIN + " " + query);
             while (rs.next()) {
                 String name = rs.getString("QUERY PLAN");
-              //  System.out.println(name);
+                //  System.out.println(name);
                 String[] splitCost = name.split("\\.\\.");
                 String[] cost1 = splitCost[1].split(" ");
                 String finalCost = cost1[0];
@@ -40,21 +40,67 @@ public class SelectQueryAnalysis {
         return queryCost;
     }
 
-    public static List<Query> getAllqueryAttiributes(String fileName){
-        List<Query> queryInfo=new ArrayList<>();
-        int queryNo=0;
-        List<String> queryList=UtlityClass.getInputFromFile(Configuration.SELECT_QUERY_TXT_FILE);
-        for (int i=0;i<queryList.size();i++){
-            String queryId="Q"+ (++queryNo);
-            String query=queryList.get(i);
-            Double queryCost=findQueryCost(query);
-            List<String> tableRequired = findTableRequired(query);
-            queryInfo.add(new Query(queryId,query,queryCost,tableRequired));
+    public static List<Query> getAllqueryAttiributes(String fileName) {
+        List<Query> queryInfo = new ArrayList<>();
+        int queryNo = 0;
+        List<String> queryList = UtlityClass.getInputFromFile(fileName);
+        Double totalFrequencyCost=0.0;
+        for (int i = 0; i < queryList.size(); i++) {
+            String queryId = "Q" + (++queryNo);
+            String[] split = queryList.get(i).split("#");
+            String query = null;
+            Integer frequency = 1;
+
+            /*
+            Validation for query format
+            */
+
+            try {
+                 query = split[0].toLowerCase();
+                frequency = Integer.valueOf(split[1]);
+            }catch (ArrayIndexOutOfBoundsException e){
+                System.out.println(queryId + "is not specified in format... format is query#frequencyof Query");
+                System.out.println("This query will be disgarded");
+                e.printStackTrace();
+                continue;
+            }
+            Double queryCost = findQueryCost(query);
+            List<String> allTables = findAllTableOfDatabase();
+            List<String> tableRequiredByQuery = findTableRequiredByquery(query, allTables);
+            List<Query> updateQueryList = findUpdateQueryReleted(tableRequiredByQuery);
+            Double weightOfQuery = findWeightOfQuery(frequency, queryCost);
+            totalFrequencyCost=totalFrequencyCost+weightOfQuery;
+            queryInfo.add(new Query(queryId, query, queryCost, tableRequiredByQuery, frequency, updateQueryList, weightOfQuery, false));
+        }
+        for(int i=0;i<queryInfo.size();i++){
+            Query query=queryInfo.get(i);
+            queryInfo.get(i).setQueryCost(query.getWeight()/totalFrequencyCost);
         }
         return queryInfo;
     }
-    public static void viewQueryListInfo(List<Query> queryList){
-        for (int i=0;i<queryList.size();i++){
+
+    private static Double findWeightOfQuery(  Integer frequency, Double queryCost) {
+        // if needed improvement let me know
+        return frequency*queryCost;
+    }
+
+    private static List<Query> findUpdateQueryReleted(List<String> tableRequiredByQuery) {
+        //yet to implement
+        return null;
+    }
+
+    private static List<String> findTableRequiredByquery(String query, List<String> allTables) {
+        List<String> tableList = new ArrayList<>();
+        for (int i = 0; i < allTables.size(); i++) {
+            if (query.contains(allTables.get(i))) {
+                tableList.add(allTables.get(i));
+            }
+        }
+        return tableList;
+    }
+
+    public static void viewQueryListInfo(List<Query> queryList) {
+        for (int i = 0; i < queryList.size(); i++) {
             System.out.println(queryList.get(i).toString());
         }
     }
